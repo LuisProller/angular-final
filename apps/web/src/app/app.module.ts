17a61d0919c8163b1feeb32c0200e729516b6c37
Luis Dutra <luisfgdutra@gmail.com>
2022-06-21 23:15:23 +0000

    criado menu principal

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { TabMenuModule } from 'primeng/tabmenu';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -10,6 +12,7 @@ import { HomeComponent } from './components/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    TabMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- apps/web/src/app/components/home/home.component.ts
+++ apps/web/src/app/components/home/home.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MenuItem } from 'primeng/api';
+
 @Component({
   selector: 'cefwm-angular-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  public items: MenuItem[] = [
+    {
+      label: 'Filmes',
+      icon: 'pi pi-play',
+      routerLink: [ '/home' ],
+    },
+  ];
 
   constructor() { }
 
